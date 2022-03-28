790564164ecc8890124e079561212cc1acb878a8
dostonbokhodirov <boxodirov.doston@gmail.com>
2022-03-28 18:29:41 +0500

    change GlobalExceptionHandler.java

--- src/main/java/uz/learn/learningcentre/exceptions/GlobalExceptionHandler.java
+++ src/main/java/uz/learn/learningcentre/exceptions/GlobalExceptionHandler.java
@@ -3,9 +3,7 @@ package uz.learn.learningcentre.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import uz.learn.learningcentre.dto.grouping.GroupingDto;
 import uz.learn.learningcentre.response.AppErrorDto;
 import uz.learn.learningcentre.response.DataDto;
 import uz.learn.learningcentre.response.ResponseEntity;
@@ -27,10 +25,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {NotFoundException.class})
     public ResponseEntity<DataDto<AppErrorDto>> handle404(RuntimeException e, WebRequest webRequest) {
-        @ResponseStatus(HttpStatus.NOT_FOUND)
-        public ResponseEntity<DataDto<GroupingDto>> handle404 (NotFoundException e ){
-            return new ResponseEntity<>
-                    (new DataDto<>(new AppErrorDto(HttpStatus.NOT_FOUND, e.getMessage(), webRequest)));
-        }
-
+        return new ResponseEntity<>
+                (new DataDto<>(new AppErrorDto(HttpStatus.NOT_FOUND, e.getMessage(), webRequest)));
     }
+}
